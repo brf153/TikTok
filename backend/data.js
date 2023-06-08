a9a4d208291de27053ef54381a87d0cd00ed3fff
brf153 <153hsb@gmail.com>
2023-06-08 16:46:46 +0530

    built frontend

--- backend/data.js
+++ backend/data.js
@@ -11,11 +11,11 @@ const Data = [
 {
     url:"./videos/pexels-cottonbro-studio-3704253-2160x4096-50fps.mp4",
     channel: "@ThisisBhandari",
-    song: "One of These Nights -- Eagles",
-    likes: "3456",
-    messages: "321",
-    description: "This is a MERN stack application",
-    shares: "500"
+    song: "Hotel California -- Eagles",
+    likes: "534",
+    messages: "123",
+    description: "Created by @ThisisBhandari -- Folow me on Twitter",
+    shares: "129"
 }
 ]
 
